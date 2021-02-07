ed113768b0ad9bb485874197d06e9eb14fa2e701
István Berta <istvan.berta@gmail.com>
2021-02-07 19:11:31 +0100

    Create config object.

---
+++ config.js
@@ -0,0 +1,18 @@
+let config = {
+    ringtimes: [
+        {time: "7:40", message: "első csengetés"},
+        {time: "7:50", message: "1. óra"},
+        {time: "8:35", message: "szünet"},
+        {time: "8:40", message: "2. óra"},
+        {time: "9:25", message: "tízórai szünet"},
+        {time: "9:35", message: "3. óra"},
+        {time: "10:20", message: "nagyszünet"},
+        {time: "10:35", message: "4. óra"},
+        {time: "11:20", message: "szünet"},
+        {time: "11:25", message: "5. óra"},
+        {time: "12:10", message: "szünet"},
+        {time: "12:15", message: "6. óra"},
+        {time: "13:00", message: ""},
+    ],
+    ringtone: "https://cdn.istvanberta.eu/sonorilo/kygo-firestone.mp3",
+}
